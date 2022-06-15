01cbdbdfc2f4e3b3eba894ae0d82bee5d5e78861
dmitry_kulbed <1>
2022-06-15 12:40:28 +0300

    added routes for pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {CongratulationToastsComponent} from './pages/congratulation-toasts/congratulation-toasts.component';
+import {MainComponent} from './pages/main/main.component';
+import {SignupComponent} from './pages/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'sign-up', component: SignupComponent},
+  {path: 'main', component: MainComponent},
+  {path: 'congratulation-toasts', component: CongratulationToastsComponent},
+  {path: '', redirectTo: 'sign-up', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
