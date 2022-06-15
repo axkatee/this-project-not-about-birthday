629b202e60d1517920ea95d3603323a2c5d3ccd7
dmitry_kulbed <1>
2022-06-15 17:59:38 +0300

    added congratulations toasts

--- src/app/consts.ts
+++ src/app/consts.ts
@@ -3,3 +3,9 @@ export const enum Pages {
   'SignUp' = 'sign-up',
   'CongratulationToasts' = 'congratulation-toasts'
 }
+
+export const enum AnimationClasses {
+  'LeftWineglass' = 'icon-left-animation',
+  'RightWineglass' = 'icon-right-animation',
+  'TextContainer' = 'toast-jump-text-animation',
+}
--- src/app/pages/congratulation-toasts/congratulation-toasts.component.ts
+++ src/app/pages/congratulation-toasts/congratulation-toasts.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
+import {AnimationClasses} from '@consts';
+import {CongratulationToasts} from '../../mock-data/mock-data';
 
 @Component({
   selector: 'app-congratulation-toasts',
@@ -6,10 +8,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./congratulation-toasts.component.scss']
 })
 export class CongratulationToastsComponent implements OnInit {
+  public toastMessage = '';
+  private isAnimationEnds = true;
 
-  constructor() { }
+  constructor(
+    private renderer: Renderer2
+  ) { }
+
+  @ViewChild('leftWineglass') wineglassLeft!: ElementRef;
+  @ViewChild('rightWineglass') wineglassRight!: ElementRef;
+  @ViewChild('toastText') toastText!: ElementRef;
 
   ngOnInit(): void {
+    this.toastMessage = this.getRandomToast();
+  }
+
+  getRandomToast(): string {
+    const maxNumber = CongratulationToasts.length;
+    return CongratulationToasts[Math.floor(Math.random() * maxNumber)]
+  }
+
+  getToast(): void {
+    if (!this.isAnimationEnds) return;
+    this.isAnimationEnds = false;
+
+    this.renderer.addClass(this.wineglassLeft.nativeElement, AnimationClasses.LeftWineglass)
+    this.renderer.addClass(this.wineglassRight.nativeElement, AnimationClasses.RightWineglass)
+    this.renderer.addClass(this.toastText.nativeElement, AnimationClasses.TextContainer)
+
+    setTimeout(() => {
+      this.toastMessage = this.getRandomToast();
+    }, 660)
+
+    setTimeout(() => {
+      this.renderer.removeClass(this.wineglassLeft.nativeElement, AnimationClasses.LeftWineglass)
+      this.renderer.removeClass(this.wineglassRight.nativeElement, AnimationClasses.RightWineglass)
+      this.renderer.removeClass(this.toastText.nativeElement, AnimationClasses.TextContainer)
+      this.isAnimationEnds = true;
+    }, 1000);
   }
 
 }
