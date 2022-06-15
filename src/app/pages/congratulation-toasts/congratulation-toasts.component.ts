ae377c58f0560002b837fc172ffb4fd8a70eb6b0
dmitry_kulbed <1>
2022-06-15 12:21:44 +0300

    Implemented project structure

---
+++ src/app/pages/congratulation-toasts/congratulation-toasts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-congratulation-toasts',
+  templateUrl: './congratulation-toasts.component.html',
+  styleUrls: ['./congratulation-toasts.component.scss']
+})
+export class CongratulationToastsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
